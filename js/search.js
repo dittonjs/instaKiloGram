028ca26d686107ef6e9ff5e6de6bfc0f42f47047
dittonjs <jditton.atomic@gmail.com>
2015-09-21 20:22:14 -0600

    added javascript the sea4rch page

--- js/search.js
+++ js/search.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
   $(".save-search-button").unbind("click").bind("click", saveSearch);
+  $(".search-field").unbind("keypress").bind("keypress", search);
 });
 
 function saveSearch(e){
+  e.preventDefault();
+  var text = $(".search-field").val();
+  if(text.length == 0) return;
+  $(".saved-searches").append("<div class='saved-search-label saved-search'>You saved a search named "+text+"</div>")
   console.log(e);
 }
 
 function search(e){
-  console.log(e);
+  if(e.keyCode == 13){  
+    var text = $(".search-field").val();
+    $(".results-panel").append("<div class='results-label search-result'>This is what would show up if you searched for "+text+"</div>")
+  } 
 }
