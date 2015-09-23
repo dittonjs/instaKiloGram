90321276ecc7a1326544d28422dbd1b41e1d39fb
dittonjs <jditton.atomic@gmail.com>
2015-09-22 20:01:10 -0600

    navigates from save search to the results

--- js/search.js
+++ js/search.js
@@ -7,13 +7,20 @@ function saveSearch(e){
   e.preventDefault();
   var text = $(".search-field").val();
   if(text.length == 0) return;
-  $(".saved-searches").append("<div class='saved-search-label saved-search'>You saved a search named "+text+"</div>")
+  $(".saved-searches").append("<div class='saved-search-label saved-search'>You saved a search named "+text+"</div>");
+  $(".saved-search").unbind("click").bind("click", openSavedSearch)
   console.log(e);
 }
 
 function search(e){
   if(e.keyCode == 13){  
-    var text = $(".search-field").val();
-    $(".results-panel").append("<div class='results-label search-result'>This is what would show up if you searched for "+text+"</div>")
+    var text = $(".search-field").val();$(".results-panel").append("<div class='results-label search-result'>This is what would show up if you searched for "+text+"</div>");
+    
   } 
+}
+
+function openSavedSearch(e){
+  var text = e.target.textContent.split(" ");
+  text = text[text.length -1];
+  $(".results-panel").append("<div class='results-label search-result'>You clicked a save search called "+text+"</div>");
 }
