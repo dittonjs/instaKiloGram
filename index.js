bd7aa0b8a27ea46235540b678262cac6b90c2dc8
dittonjs <jditton.atomic@gmail.com>
2015-10-14 12:37:03 -0600

    adds views and templates

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 var express = require('express'), app = express(), port = 3000;
+var expbars = require('express-handlebars');
+
+app.engine('handlebars', expbars({defaultLayout: "base"}));
+app.set("view engine", "handlebars");
 
 app.get('/', function(req, res){
-  res.setHeader("Content-Type", "text/html");
-  res.sendFile(__dirname + '/public/html/home.html');
+  res.render('home', {
+    title: "This is weird",
+    name: "Joseph",
+  })
 });
 
-app.get('/dashboard', function(req, res){
-  res.sendFile(__dirname + "/public/html/dashboard.html");
-});
+// app.get('/dashboard', function(req, res){
+//   res.sendFile(__dirname + "/public/html/dashboard.html");
+// });
 
-app.get('/search', function(req, res){
-  res.sendFile(__dirname + "/public/html/search.html");
-});
+// app.get('/search', function(req, res){
+//   res.sendFile(__dirname + "/public/html/search.html");
+// });
 
-app.get('/profile', function(req, res){
-  res.sendFile(__dirname + "/public/html/profile.html");
-});
+// app.get('/profile', function(req, res){
+//   res.sendFile(__dirname + "/public/html/profile.html");
+// });
 
 app.use(express.static(__dirname + '/public'));
 
